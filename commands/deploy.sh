f2d5af828af97f90271046e44832d7308add6f9d
dmytro.kaminskyi92@gmail.com <dmytro.kaminskyi92@gmail.com>
2020-09-06 00:10:42 +0300

    fix deploy script

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -2,6 +2,6 @@
 
 git pull origin master &&
 docker-compose up -d --build &&
-docker exec backend migrate &&
-docker exec backend collecstatic --noinput &&
+docker exec backend python ./src/manage.py migrate &&
+docker exec backend python ./src/manage.py collecstatic --noinput &&
 docker exec -it backend pytest ./src/tests -s -x --cov=src --cov-report html
